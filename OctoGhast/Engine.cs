78e167550c938ea0ff8cea182e5b6f4f8b1c7ce5
Alexander Somerville-Cox <alexander.somerville.cox@gmail.com>
2013-10-22 22:54:43 +0100

    Initial commit.
    Hello @

---
+++ OctoGhast/Engine.cs
@@ -0,0 +1,31 @@
+﻿using libtcod;
+
+namespace OctoGhast
+{
+    public class Engine
+    {
+        int ScreenWidth { get; set; }
+        int ScreenHeight { get; set; }
+
+        public Engine(int screenWidth, int screenHeight) {
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+        }
+
+        public void Setup() {
+            // TODO: Fix TCODConsole to be friendlier.
+            TCODConsole.setCustomFont("celtic_garamond_10x10_gs_tc.png", (int) (TCODFontFlags.LayoutTCOD | TCODFontFlags.Grayscale));
+            TCODConsole.initRoot(ScreenWidth, ScreenHeight, "OctoGhast", false);
+            TCODSystem.setFps(20);
+        }
+
+        public void Tick() {
+            var root = TCODConsole.root;
+
+            root.setBackgroundColor(TCODColor.black);
+            root.setForegroundColor(TCODColor.white);
+            root.putChar(1, 1, '@');
+            TCODConsole.flush();
+        }
+    }
+}
