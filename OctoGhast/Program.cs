d1a4efb9380fa7676c39b46d334da9595fd31b58
LambdaSix <LambdaSix@users.noreply.github.com>
2013-10-22 22:54:43 +0100

    Initial commit.
    Hello @

---
+++ OctoGhast/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libtcod;
+
+namespace OctoGhast
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var engine = new Engine(80, 25);
+            engine.Setup();
+
+            while (true) {
+                engine.Tick();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
