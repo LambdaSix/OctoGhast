8629bf4ba6ef2ad42d8373ba8f2c1afc7bf2da67
LambdaSix <LambdaSix@users.noreply.github.com>
2013-10-22 23:20:45 +0100

    Holy moving @'s batman.

--- OctoGhast/Engine.cs
+++ OctoGhast/Engine.cs
@@ -1,4 +1,6 @@
-﻿using libtcod;
+﻿using System.Collections;
+using System.Collections.Generic;
+using libtcod;
 
 namespace OctoGhast
 {
@@ -7,15 +9,20 @@ namespace OctoGhast
         int ScreenWidth { get; set; }
         int ScreenHeight { get; set; }
 
-        int PlayerX { get; set; }
-        int PlayerY { get; set; }
+        private Object _player;
+
+        TCODConsole Buffer { get; set; }
+        private IList<Object> _objects = new List<Object>();
 
         public Engine(int screenWidth, int screenHeight) {
             ScreenWidth = screenWidth;
             ScreenHeight = screenHeight;
 
-            PlayerX = ScreenWidth/2;
-            PlayerY = ScreenHeight/2;
+            _player = new Object(ScreenWidth/2, ScreenHeight/2, '@', TCODColor.white);
+            var npc = new Object(ScreenWidth/2 - 5, ScreenHeight/2, '@', TCODColor.yellow);
+
+            _objects.Add(_player);
+            _objects.Add(npc);
         }
 
         public void Setup() {
@@ -23,18 +30,25 @@ namespace OctoGhast
             TCODConsole.setCustomFont("celtic_garamond_10x10_gs_tc.png", (int) (TCODFontFlags.LayoutTCOD | TCODFontFlags.Grayscale));
             TCODConsole.initRoot(ScreenWidth, ScreenHeight, "OctoGhast", false);
             TCODSystem.setFps(20);
+            Buffer = new TCODConsole(ScreenWidth, ScreenHeight);
         }
 
         public void Tick() {
             Draw(TCODConsole.root);
-            TCODConsole.root.putChar(PlayerX, PlayerY, ' ');
+
+            foreach (var obj in _objects) {
+                obj.Clear(Buffer);
+            }
+
             HandleInput();
         }
 
         public void Draw(TCODConsole root) {
-            root.setBackgroundColor(TCODColor.black);
-            root.setForegroundColor(TCODColor.white);
-            root.putChar(PlayerX, PlayerY, '@');
+            foreach (var obj in _objects) {
+                obj.Draw(Buffer);
+            }
+
+            TCODConsole.blit(Buffer, 0, 0, ScreenWidth, ScreenHeight, root, 0, 0);
             TCODConsole.flush();
         }
 
@@ -42,28 +56,26 @@ namespace OctoGhast
             var key = TCODConsole.waitForKeypress(true);
 
             // TODO: Replace with a mapping system.
-            switch (key.KeyCode)
-            {
-                case TCODKeyCode.Up:
-                    {
-                        PlayerY -= 1;
-                        break;
-                    }
-                case TCODKeyCode.Down:
-                    {
-                        PlayerY += 1;
-                        break;
-                    }
-                case TCODKeyCode.Left:
-                    {
-                        PlayerX -= 1;
-                        break;
-                    }
-                case TCODKeyCode.Right:
-                    {
-                        PlayerX += 1;
-                        break;
-                    }
+            switch (key.KeyCode) {
+                case TCODKeyCode.Up: {
+                    _player.Move(0, -1);
+                    break;
+                }
+                case TCODKeyCode.Down: {
+                    _player.Move(0, 1);
+
+                    break;
+                }
+                case TCODKeyCode.Left: {
+                    _player.Move(-1, 0);
+
+                    break;
+                }
+                case TCODKeyCode.Right: {
+                    _player.Move(1, 0);
+
+                    break;
+                }
             }
         }
     }
---
+++ OctoGhast/Object.cs
@@ -0,0 +1,33 @@
+﻿using libtcod;
+
+namespace OctoGhast
+{
+    public class Object
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Icon { get; set; }
+        public TCODColor Color { get; set; }
+
+        public Object(int x, int y, char icon, TCODColor color) {
+            X = x;
+            Y = y;
+            Icon = icon;
+            Color = color;
+        }
+
+        public void Move(int deltaX, int deltaY) {
+            X += deltaX;
+            Y += deltaY;
+        }
+
+        public void Draw(TCODConsole buffer) {
+            buffer.setForegroundColor(Color);
+            buffer.putChar(X, Y, Icon);
+        }
+
+        public void Clear(TCODConsole buffer) {
+            buffer.putChar(X, Y, ' ');
+        }
+    }
+}
