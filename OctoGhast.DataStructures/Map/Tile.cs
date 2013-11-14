6625579fdb8d4c0a0916d91ae1f0bde77666c022
LambdaSix <LambdaSix@users.noreply.github.com>
2013-11-14 21:36:05 +0000

    Move Tile into OctoGhast.DataStructures for later expansion

---
+++ OctoGhast.DataStructures/Map/Tile.cs
@@ -0,0 +1,18 @@
+﻿namespace OctoGhast.DataStructures.Map
+{
+    public interface ITile
+    {
+        bool IsVisible { get; set; }
+        bool IsWalkable { get; set; }
+        int Glyph { get; set; }
+    }
+
+    /* Implementations */
+
+    public class Tile : ITile
+    {
+        public bool IsVisible { get; set; }
+        public bool IsWalkable { get; set; }
+        public int Glyph { get; set; }
+    }
+}
