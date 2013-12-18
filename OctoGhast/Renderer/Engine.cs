a9e1de0528c0178371ed4669ffbac328733a5152
Alexander Somerville-Cox <alexander.somerville.cox@gmail.com>
2013-12-18 23:30:06 +0000

    Tidy up Engine.cs
    Possibly some small fixes that maybe should have made it in earlier.
    Oops.

--- OctoGhast/Renderer/Engine.cs
+++ OctoGhast/Renderer/Engine.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using libtcod;
 using OctoGhast.DataStructures.Entity;
 using OctoGhast.DataStructures.Map;
 using OctoGhast.Entity;
-using OctoGhast.MapGeneration;
 using OctoGhast.MapGeneration.Dungeons;
 using OctoGhast.Spatial;
 
@@ -15,20 +12,12 @@ namespace OctoGhast.Renderer
 {
     public class Engine
     {
-        private GameMap _map;
-        private Camera _camera;
-
         // Because we access black /so much/ per frame, memoize it to avoid going
         // across the P/Invoke border to libTCOD
         private static readonly TCODColor ColorBlack = TCODColor.black;
-
-        private TCODConsole Screen { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-
-        private ICollection<GameObject> _objects = new List<GameObject>();
-
-        public Player Player { get; set; }
+        private readonly GameMap _map;
+        private readonly ICollection<GameObject> _objects = new List<GameObject>();
+        private Camera _camera;
         private bool _dirtyFov;
 
         public Engine(int width, int height) {
@@ -39,7 +28,7 @@ namespace OctoGhast.Renderer
 
             var mapGen = new BSPDungeonGenerator {
                 PlayerPlacementFunc = (rect) => playerPosition = rect.Center,
-                MobilePlacementFunc = (rect) => _objects.Add(new GameObject(rect.Center, 'c', TCODColor.orange)),
+                MobilePlacementFunc = (rect) => _objects.Add(new Mobile(rect.Center, 'c', TCODColor.orange)),
             };
 
             _map = new GameMap(Width*3, Height*3);
@@ -48,9 +37,21 @@ namespace OctoGhast.Renderer
             _map.InvalidateMap();
 
             Player = new Player(playerPosition, '@', TCODColor.amber);
-            Player.MoveTo(playerPosition, _map);
+            Player.MoveTo(playerPosition, _map, Enumerable.Empty<IMobile>());
         }
 
+        
+
+        private TCODConsole Screen { get; set; }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Player Player { get; set; }
+
+        /// <summary>
+        /// Initialize the Engine and window.
+        /// </summary>
         public void Setup() {
             TCODConsole.setCustomFont("celtic_garamond_10x10_gs_tc.png", (int) TCODFontFlags.LayoutTCOD);
             TCODConsole.initRoot(Width, Height, "OctoGhast", false);
@@ -62,9 +63,12 @@ namespace OctoGhast.Renderer
             _camera = new Camera(Player.Position, new Rect(80, 25), _map.MapArray.Bounds);
         }
 
+        /// <summary>
+        /// Render a single frame and wait for input.
+        /// </summary>
         public void Update() {
             Render(Screen);
-            var key = TCODConsole.waitForKeypress(false);
+            TCODKey key = TCODConsole.waitForKeypress(false);
             ProcessKey(key);
         }
 
@@ -104,31 +108,28 @@ namespace OctoGhast.Renderer
             if (key.KeyCode == TCODKeyCode.Down) {
                 _dirtyFov = Player.MoveTo(new Vec(Player.Position.X, Player.Position.Y + 1), _map, GetMobilesInView());
             }
+
+            // If we managed to move the player, then update the camera to the current location.
+            if (_dirtyFov) {
+                _camera.MoveTo(Player.Position);
+            }
         }
 
-        private Camera _camera;
-
         public void Render(TCODConsole buffer) {
-            var fovChanged = _camera.MoveTo(Player.Position);
-
             if (_dirtyFov) {
                 _map.CalculateFov(Player.Position, 8);
                 _dirtyFov = false;
             }
 
-            //if (fovChanged)
-                //_map.InvalidateMap();
-
-            var frustumView = _map.GetFrustumView(_camera);
+            Array2D<Tile> frustumView = _map.GetFrustumView(_camera);
 
             for (int x = 0; x < _camera.Width; x++) {
                 for (int y = 0; y < _camera.Height; y++) {
-                    var worldCoords = _camera.ToWorldCoords(new Vec(x, y));
+                    Vec worldCoords = _camera.ToWorldCoords(new Vec(x, y));
 
                     buffer.putCharEx(x, y, ' ', ColorBlack, ColorBlack);
 
-                    if (_map.IsExplored(worldCoords.X, worldCoords.Y))
-                    {
+                    if (_map.IsExplored(worldCoords.X, worldCoords.Y)) {
                         buffer.putCharEx(x, y, frustumView[x, y].Glyph, TCODColor.darkGrey, ColorBlack);
                     }
 
@@ -148,12 +149,13 @@ namespace OctoGhast.Renderer
             Player.Draw(buffer, _camera.ToViewCoords(Player.Position));
 
             buffer.setForegroundColor(TCODColor.white);
-            var playerVis = _camera.ToViewCoords(Player.Position);
+            Vec playerVis = _camera.ToViewCoords(Player.Position);
 
             buffer.print(0, 24, String.Format("P: {0},{1}; VP: {2},{3}", Player.Position.X, Player.Position.Y,
                 playerVis.Y, playerVis.X));
+            buffer.print(0,23, String.Format("C: {0},{1}", _camera.CameraCenter.X, _camera.CameraCenter.Y));
 
-            buffer.print(0, 23, String.Format("FL: {0}", TCODSystem.getFps()));
+            buffer.print(0, 22, String.Format("FL: {0}", TCODSystem.getFps()));
 
             TCODConsole.flush();
         }
