1b1dd3480970cf6f53adc74a00af99eb5069d67f
LambdaSix <LambdaSix@users.noreply.github.com>
2013-12-18 22:33:12 +0000

    Reorganise classes around the player and gameobjects.
    Introduces Mobile, the base for Player and any NPC's/Monsters.
    Also pass a list of the current objects to the MoveTo() so we
    can do collision checks.

--- OctoGhast.DataStructures/Entity/GameObject.cs
+++ OctoGhast.DataStructures/Entity/GameObject.cs
@@ -5,41 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using libtcod;
 using OctoGhast.Spatial;
-using OctoGhast.DataStructures.Map;
 
 namespace OctoGhast.DataStructures.Entity
 {
-    public class GameObject
+    public interface IGlyph
     {
-        public Vec Position { get; private set; }
-        public char Glyph { get; private set; }
-        public TCODColor Color { get; private set; }
+        int Icon { get; set; }
+    }
 
-        public GameObject(Vec position, char glyph, TCODColor color) {
+    public class Glyph : IGlyph
+    {
+        public int Icon { get; set; }
+
+        public Glyph(int icon) {
+            Icon = icon;
+        }
+    }
+
+    public interface IGameObject
+    {
+        Vec Position { get; }
+        IGlyph Glyph { get; }
+
+        void Draw(TCODConsole buffer, Vec drawPos);
+    }
+
+    public class GameObject : IGameObject
+    {
+        public Vec Position { get; protected set; }
+        public IGlyph Glyph { get; protected set; }
+        public TCODColor Color { get; protected set; }
+
+        public GameObject(Vec position, IGlyph glyph, TCODColor color) {
             Position = position;
             Glyph = glyph;
             Color = color;
         }
 
-        public bool MoveTo(Vec position, GameMap gameMap) {
-            var mapTile = gameMap.MapArray[position.X, position.Y];
-            var mapWalkable = mapTile.IsWalkable;
-
-            if (mapWalkable) {
-                Position = position;
-                return true;
-            }
-            return false;
+        public GameObject(Vec position, int glyph, TCODColor color) : this(position, new Glyph(glyph), color) {
+            
         }
 
-        public void Draw(TCODConsole buffer, Vec drawPosition)
+        public virtual void Draw(TCODConsole buffer, Vec drawPosition)
         {
-            buffer.putCharEx(drawPosition.X, drawPosition.Y, Glyph, Color, TCODColor.black);
-        }
-
-        public void Undraw(TCODConsole buffer, Vec drawPosition) {
-
-            buffer.putChar(drawPosition.X, drawPosition.Y, ' ');
+            buffer.putCharEx(drawPosition.X, drawPosition.Y, Glyph.Icon, Color, TCODColor.black);
         }
     }
 }
