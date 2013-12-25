9fc6738f9dc63c803f3dfc1f9e246c52635e14d4
Alexander Somerville <alexander.somerville.cox@gmail.com>
2013-12-25 11:57:31 +0000

    Pull some public members of Camera up to ICamera

--- OctoGhast.DataStructures/Renderer/ICamera.cs
+++ OctoGhast.DataStructures/Renderer/ICamera.cs
@@ -4,14 +4,22 @@ namespace OctoGhast.DataStructures.Renderer
 {
     public interface ICamera
     {
-        Vec CameraPosition { get; set; }
-        Rect Dimensions { get; set; }
-        Rect MapSize { get; set; }
-        int MapWidth { get; }
-        int MapHeight { get; }
-        int Width { get; }
-        int Height { get; }
-        Vec ToViewCoords(Vec position);
-        bool MoveTo(Vec position);
+	    Vec CameraPosition { get; set; }
+
+	    /// <summary>
+	    /// Center point of the camera.
+	    /// </summary>
+	    Vec CameraCenter { get; }
+
+	    Rect Dimensions { get; set; }
+	    Rect MapSize { get; set; }
+	    int MapWidth { get; }
+	    int MapHeight { get; }
+	    int Width { get; }
+	    int Height { get; }
+	    Rect ViewFrustum { get; }
+	    Vec ToWorldCoords(Vec position);
+	    Vec ToViewCoords(Vec position);
+	    bool MoveTo(Vec position);
     }
 }
