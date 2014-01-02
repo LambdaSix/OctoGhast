6007d5bc0fca44a65e835e74154b7463a319db8f
Alexander Somerville-Cox <alexander.somerville.cox@gmail.com>
2014-01-02 00:54:50 +0000

    Add simple framework for combat, needs expansion

--- OctoGhast/Entity/Player.cs
+++ OctoGhast/Entity/Player.cs
@@ -11,14 +11,36 @@ namespace OctoGhast.Entity
         // TODO: Player specific things. (If any?)
     }
 
+    public interface IWeapon
+    {
+        int Range { get;  }
+        int Damage { get;  }
+        int Durability { get;  }
+        string Name { get; }
+    }
+
+    public class Dagger : IWeapon
+    {
+        public int Range { get { return 1; }  }
+        public int Damage { get { return 8; } }
+        public int Durability { get { return 4; } }
+
+        public string Name { get { return "Dagger"; }}
+    }
+
     public class Player : Mobile, IPlayer
     {
-        public Player(Vec position, char glyph, TCODColor color) : base(position, glyph, color, "Player") { }
+        public IWeapon ActiveWeapon { get; set; }
 
-        public override bool Attack(IMobile other)
+        public Player(Vec position, char glyph, TCODColor color) : base(position, glyph, color, "Player") {
+            ActiveWeapon = new Dagger();
+        }
+
+        public override void Attack(IMobile other)
         {
-            Console.WriteLine("Bumped into {0}, but I have no weapon :(", other.Name);
-            return true;
+            if (IsInRange(other, ActiveWeapon.Range)) {
+                Console.WriteLine("You strike the {0} for {1} points of damage with your {2}!", other.Name, ActiveWeapon.Damage, ActiveWeapon.Name);
+            }
         }
     }
 }
