8fc979ea81ea631f501573801f8145ecddaf8dc1
LambdaSix <LambdaSix@users.noreply.github.com>
2014-03-22 23:13:39 +0000

    Add Size object to Spatial project.

---
+++ OctoGhast.Spatial/Size.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OctoGhast.Spatial
+{
+    [Serializable]
+    public struct Size
+    {
+        private readonly int _width;
+        private readonly int _height;
+
+        public Size(int width, int height) {
+            Width = width;
+            _height = height;
+        }
+
+        public Size(Size size) {
+            Width = size.Width;
+            _height = size._height;
+        }
+
+        public int Width {
+            get { return _width; }
+        }
+
+        public int Height {
+            get { return _height; }
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null)
+                return false;
+
+            if (obj is Size)
+                return Equals((Size)obj);
+            
+            return false;          
+        }
+
+        public bool Equals(Size other) {
+            return (Width == other.Width && Height == other.Height);
+        }
+
+        public override int GetHashCode() {
+            return Width.GetHashCode() + Height.GetHashCode();
+        }
+
+        public override string ToString() {
+            return String.Format("{0},{1}", Width, Height);
+        }
+
+        public static bool operator ==(Size left, Size right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Size left, Size right) {
+            return !(left == right);
+        }
+    }
