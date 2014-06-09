81771a662752dddcf091a37bf5848e0351fdb799
LambdaSix <LambdaSix@users.noreply.github.com>
2014-06-09 22:53:49 +0100

    Simple container class for 2D lightmaps

---
+++ OctoGhast.DataStructures/Lighting/LightMap.cs
@@ -0,0 +1,26 @@
+﻿namespace OctoGhast.DataStructures.Lighting
+{
+    public class LightMap<T>
+    {
+        private readonly T[] _map;
+        private readonly int _width;
+        private readonly int _height;
+
+        public LightMap(int height, int width) {
+            _map = new T[width*height];
+            _height = height;
+            _width = width;
+        }
+
+        public T this[int x, int y] {
+            get {
+                var n = (x + (_width*y));
+                return _map[n];
+            }
+            set {
+                var n = (x + (_width*y));
+                _map[n] = value;
+            }
+        }
+    }
+}
