c92874aaae2435d1c11e1a40fe5694408fc979ea
LambdaSix <alexander.somerville.cox@gmail.com>
2014-06-29 18:01:27 +0100

    A component is a container of events that can be raised on UI elements.
    It can also have items scheduled to run after a delay. Or at least the nearest tick
    to the delay.

---
+++ OctoGhast.UserInterface/Core/Messages/MouseEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using libtcod;
+using OctoGhast.Spatial;
+
+namespace OctoGhast.UserInterface.Core.Messages
+{
+    public enum MouseButton
+    {
+        None = 0,
+        Left = 1,
+        Middle = 2,
+        Right = 3
+    }
+
+    public class MouseData
+    {
+        public Vec Position { get; private set; }
+        public Vec ScreenPosition { get; private set; }
+        public MouseButton MouseButton { get; private set; }
+
+        public MouseData(MouseButton button, Vec position, Vec screenPosition) {
+            MouseButton = button;
+            Position = position;
+            ScreenPosition = screenPosition;
+        }
+
+        public MouseData(TCODMouseData tcodMouseData) {
+            Position = new Vec(tcodMouseData.CellX, tcodMouseData.CellY);
+            ScreenPosition = new Vec(tcodMouseData.PixelX, tcodMouseData.PixelY);
+            MouseButton = MouseButton.None;
+
+            if (tcodMouseData.LeftButton)
+                MouseButton = MouseButton.Left;
+            if (tcodMouseData.MiddleButton)
+                MouseButton = MouseButton.Middle;
+            if (tcodMouseData.RightButton)
+                MouseButton = MouseButton.Right;
+        }
+    }
+
+    public class MouseEventArgs : EventArgs
+    {
+        public MouseData MouseData { get; private set; }
+
+        public MouseEventArgs(MouseData mouseData) {
+            MouseData = mouseData;
+        }
+    }
+
+    public class MouseDragEventArgs : EventArgs
+    {
+        public Vec ScreenPosition { get; private set; }
+
+        public MouseDragEventArgs(Vec screenPosition) {
+            ScreenPosition = screenPosition;
+        }
+    }
+}
