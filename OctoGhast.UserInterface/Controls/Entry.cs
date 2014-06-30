aecce5129fe6a7d6004f3f503afd51859781bf86
LambdaSix <alexander.somerville.cox@gmail.com>
2014-06-30 22:54:20 +0100

    Add Text Entry control and supporting class
    Allows the user to type text into a control with events raised
    when the control values have changed.

---
+++ OctoGhast.UserInterface/Controls/Entry.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Reflection.Emit;
+using libtcod;
+using OctoGhast.Spatial;
+using OctoGhast.UserInterface.Core;
+using OctoGhast.UserInterface.Core.Interface;
+using OctoGhast.UserInterface.Core.Messages;
+using OctoGhast.UserInterface.Templates;
+using OctoGhast.UserInterface.Theme;
+
+namespace OctoGhast.UserInterface.Controls
+{
+    public abstract class EntryTemplate : ControlTemplate
+    {
+        public HAlign LabelAlign { get; set; }
+        public VAlign VerticalAlign { get; set; }
+        public bool HasFrameBorder { get; set; }
+        public bool ReplaceOnFirstKey { get; set; }
+        public bool CommitOnLostFocus { get; set; }
+        public bool MouseOverHighlight { get; set; }
+        public bool CanHaveKeyboardFocus { get; set; }
+        public string Label { get; set; }
+        public uint BlinkDelay { get; set; }
+        public Size AutoSizeOverride { get; set; }
+
+        public EntryTemplate() {
+            Label = "";
+            CanHaveKeyboardFocus = true;
+            MouseOverHighlight = false;
+            CommitOnLostFocus = false;
+            ReplaceOnFirstKey = false;
+            HasFrameBorder = true;
+            VerticalAlign = VAlign.Center;
+            LabelAlign = HAlign.Left;
+            BlinkDelay = 500;
+        }
+
+        public abstract int CalculateMaxCharacters();
+    }
+
+    public abstract class Entry : ControlBase
+    {
+        private bool waitingToCommitText { get; set; }
+        private bool waitingToOverwrite { get; set; }
+        private bool _cursorOn = true;
+        private Rect _labelRect;
+        private Rect _fieldRect;
+        private int _cursorY;
+        private readonly uint _blinkDelay;
+
+        protected abstract string DefaultField { get; }
+
+        public event EventHandler EntryChanged;
+
+        public string Label { get; set; }
+        public int MaximumCharacters { get; set; }
+
+        public bool CommitOnLostFocus { get; set; }
+        public bool ReplaceOnFirstKey { get; set; }
+
+        public VAlign VerticalAlign { get; set; }
+        public HAlign LabelAlign { get; set; }
+
+        public string CurrentText { get; set; }
+        public string TextInput { get; set; }
+        public int CursorPos { get; set; }
+
+        public Entry(EntryTemplate template) : base(template) {
+            Label = template.Label;
+
+            if (Size.Width < 3 || Size.Height < 3)
+                template.HasFrameBorder = false;
+
+            HasFrame = template.HasFrameBorder;
+            MaximumCharacters = template.CalculateMaxCharacters();
+
+            CommitOnLostFocus = template.CommitOnLostFocus;
+            ReplaceOnFirstKey = template.ReplaceOnFirstKey;
+            CanHaveKeyboardFocus = template.CanHaveKeyboardFocus;
+            HilightWhenMouseOver = template.MouseOverHighlight;
+
+            VerticalAlign = template.VerticalAlign;
+            LabelAlign = template.LabelAlign;
+
+            CurrentText = "";
+            waitingToCommitText = false;
+            TextInput = CurrentText;
+            _blinkDelay = template.BlinkDelay;
+
+            CalcMetrics(template);
+        }
+
+        protected abstract bool ValidateCharacter(char character);
+
+        protected abstract bool ValidateField(string entry);
+
+        public bool TrySetField(string changeTo) {
+            TextInput = changeTo;
+            return TryCommit();
+        }
+
+        public bool TryCommit() {
+            if (CurrentText == TextInput)
+                return false;
+            if (ValidateField(TextInput) && TextInput.Length <= MaximumCharacters) {
+                CurrentText = TextInput;
+
+                OnFieldChanged();
+                return true;
+            }
+            TextInput = CurrentText;
+            return false;
+        }
+
+        public override void OnSettingUp()
+        {
+            base.OnSettingUp();
+
+            // Add a schedule to blink the cursor (turn it on if off, and Vice versa)
+            AddSchedule(new Schedule(() => _cursorOn = !_cursorOn, _blinkDelay));
+        }
+
+        protected override void Redraw() {
+            base.Redraw();
+
+            if (!String.IsNullOrEmpty(Label))
+                Canvas.PrintStringAligned(_labelRect.TopLeft, Label, LabelAlign, VerticalAlign, Size);
+
+            if (waitingToOverwrite) {
+                Canvas.PrintStringAligned(_fieldRect.TopLeft, TextInput, HAlign.Left, VerticalAlign, Size,
+                    Pigments[PigmentType.ViewSelected]);
+            }
+            else {
+                Canvas.PrintStringAligned(_fieldRect.TopLeft, TextInput, HAlign.Left, VerticalAlign, Size);
+            }
+
+            if (_cursorOn && HasKeyboardFocus) {
+                int cursorX = _fieldRect.Left + CursorPos;
+                if (cursorX <= LocalRectangle.Right) {
+                    Canvas.PrintChar(cursorX, _cursorY, (char) TCODSpecialCharacter.Block1,
+                        Pigments[PigmentType.ViewSelected]);
+                }
+            }
+        }
+
+        protected virtual void OnFieldChanged() {
+            if (EntryChanged != null)
+                EntryChanged(this, EventArgs.Empty);
+        }
+
+        public override void OnKeyPressed(KeyboardData keyData) {
+            base.OnKeyPressed(keyData);
+
+            if (keyData.Character != 0 && ValidateCharacter(keyData.Character)) {
+                if (waitingToOverwrite) {
+                    TextInput = keyData.Character.ToString();
+                    CursorPos = 1;
+                    waitingToOverwrite = false;
+                }
+                else if (TextInput.Length < MaximumCharacters) {
+                    TextInput += keyData.Character;
+                    CursorPos++;
+                }
+            }
+            else if (keyData.KeyCode == TCODKeyCode.Backspace && TextInput.Length > 0) {
+                TextInput = TextInput.Substring(0, TextInput.Length - 1);
+                CursorPos--;
+            }
+            else if (keyData.KeyCode == TCODKeyCode.Enter) {
+                waitingToCommitText = true;
+                ParentWindow.ReleaseKeyboard(this);
+            }
+            else if (keyData.KeyCode == TCODKeyCode.Escape) {
+                TextInput = CurrentText;
+                waitingToCommitText = true;
+                ParentWindow.ReleaseKeyboard(this);
+            }
+        }
+
+        protected internal override void OnTakeKeyboardFocus() {
+            base.OnTakeKeyboardFocus();
+
+            waitingToCommitText = false;
+            TextInput = CurrentText;
+
+            if (ReplaceOnFirstKey)
+                waitingToOverwrite = true;
+
+            CursorPos = CurrentText.Length;
+        }
+
+        protected internal override void OnReleaseKeyboardFocus() {
+            base.OnReleaseKeyboardFocus();
+
+            if (waitingToCommitText || CommitOnLostFocus)
+                TryCommit();
+            else
+                TextInput = CurrentText;
+
+            waitingToOverwrite = false;
+        }
+
+        private void CalcMetrics(EntryTemplate template) {
+            var viewRect = LocalRectangle;
+
+            if (template.HasFrameBorder)
+                viewRect = viewRect.Inflate(-1, -1);
+
+            int remaining = viewRect.Size.Y;
+            int labelLength = template.Label.Length;
+            int fieldLength = template.CalculateMaxCharacters();
+
+            remaining -= fieldLength;
+
+            if (remaining < 0) {
+                fieldLength += remaining;
+                labelLength = 0;
+            }
+            else {
+                remaining -= labelLength;
+                labelLength += remaining - 1;
+            }
+
+            if (labelLength < 1) {
+                Label = "";
+                labelLength = 0;
+            }
+
+            _labelRect = new Rect(viewRect.TopLeft, new Size(labelLength, viewRect.Size.X));
+            _fieldRect = new Rect(_labelRect.TopRight.OffsetX(1), new Size(fieldLength, viewRect.Size.X));
+
+            switch (VerticalAlign)
+            {
+                case VAlign.Top:
+                    _cursorY = _fieldRect.Top;
+                    break;
+
+                case VAlign.Center:
+                    _cursorY = _fieldRect.Center.Y;
+                    break;
+
+                case VAlign.Bottom:
+                    _cursorY = _fieldRect.Bottom;
+                    break;
+            }
+        }
+    }
+}
---
+++ OctoGhast.UserInterface/Controls/TextEntry.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Xml.Schema;
+using OctoGhast.Spatial;
+using OctoGhast.UserInterface.Core;
+using OctoGhast.UserInterface.Templates;
+
+namespace OctoGhast.UserInterface.Controls
+{
+    [Flags]
+    public enum TextEntryValidations
+    {
+        /// <summary>
+        /// Allow all printable characters, excluding control codes.
+        /// </summary>
+        All = 15,
+
+        /// <summary>
+        /// Allow [a-zA-Z\s]
+        /// </summary>
+        Letters = 1,
+
+        /// <summary>
+        /// Allow [0-9]
+        /// </summary>
+        Numbers = 2,
+
+        /// <summary>
+        /// Allow [+-.0-9]
+        /// </summary>
+        Decimal = 4,
+
+        /// <summary>
+        /// Allow all printable symbols that are not numbers or letters.
+        /// </summary>
+        Symbols = 8,
+    }
+
+    public class TextEntryTemplate : EntryTemplate
+    {
+        public int MaximumCharacters { get; set; }
+        public TextEntryValidations Validation { get; set; }
+        public string StartingField { get; set; }
+
+        public TextEntryTemplate() {
+            MaximumCharacters = 1;
+            Validation = TextEntryValidations.All;
+
+            StartingField = "";
+        }
+
+        public override Size CalculateSize() {
+            if (AutoSizeOverride.Height > 0 && AutoSizeOverride.Width >= MaximumCharacters) {
+                return AutoSizeOverride;
+            }
+
+            if (Label == null)
+                Label = "";
+
+            int len = CanvasUtil.MeasureStr(Label);
+
+            int frameSize = 0;
+
+            if (HasFrameBorder) {
+                frameSize = 2;
+            }
+
+            return new Size(len + MaximumCharacters + 1 + frameSize, 1 + frameSize);
+        }
+
+        public override int CalculateMaxCharacters() {
+            return MaximumCharacters;
+        }
+    }
+
+    public class TextEntry : Entry
+    {
+        public TextEntryValidations Validation { get; set; }
+
+        public TextEntry(TextEntryTemplate template) : base(template) {
+            Validation = template.Validation;
+            TrySetField(template.StartingField);
+        }
+
+        protected override string DefaultField {
+            get { return ""; }
+        }
+
+        protected override bool ValidateField(string entry) {
+            return true;
+        }
+
+        protected override bool ValidateCharacter(char character) {
+            var valid = false;
+
+            if (Validation.HasFlag(TextEntryValidations.Numbers) || Validation.HasFlag(TextEntryValidations.Decimal)) {
+                if (Char.IsNumber(character)) {
+                    valid = true;
+                }
+            }
+
+            if (Validation.HasFlag(TextEntryValidations.Letters)) {
+                if (Char.IsLetter(character) || Char.IsWhiteSpace(character)) {
+                    valid = true;
+                }
+            }
+
+            if (Validation.HasFlag(TextEntryValidations.Decimal)) {
+                if (character == '+' || character == '-' || character == '.') {
+                    valid = true;
+                }
+            }
+
+            if (Validation.HasFlag(TextEntryValidations.Symbols)) {
+                if (Char.IsSymbol(character)) {
+                    valid = true;
+                }
+            }
+
+            return valid;
+        }
+    }
+}
