dfde056d11503a7e924e2b7e06c74ea1ef3dfbe9
LambdaSix <LambdaSix@users.noreply.github.com>
2014-07-22 21:29:14 +0100

    Add a Panel control

---
+++ OctoGhast.UserInterface/Controls/Panel.cs
@@ -0,0 +1,38 @@
+﻿using OctoGhast.Spatial;
+using OctoGhast.UserInterface.Templates;
+using OctoGhast.UserInterface.Theme;
+
+namespace OctoGhast.UserInterface.Controls
+{
+    public class PanelTemplate : ControlTemplate
+    {
+        /// <summary>
+        /// The size of the Panel, defaults to 1x1
+        /// </summary>
+        public Size Size { get; set; }
+
+        public PanelTemplate() {
+            HasFrameBorder = true;
+            CanHaveKeyboardFocus = false;
+            MouseOverHighlight = false;
+            Size = new Size(1, 1);
+        }
+
+        public override Size CalculateSize() {
+            return Size;
+        }
+    }
+
+    public class Panel : ControlBase
+    {
+        public Panel(PanelTemplate template) : base(template) {
+            HasFrame = template.HasFrameBorder;
+            CanHaveKeyboardFocus = template.CanHaveKeyboardFocus;
+            MouseOverHighlight = template.MouseOverHighlight;
+        }
+
+        protected override Pigment DetermineMainPigment() {
+            return Pigments[PigmentType.Window];
+        }
+    }
+}
