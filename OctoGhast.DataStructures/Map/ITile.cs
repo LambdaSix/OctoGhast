ab6a148328bca8170c5c5b3259a2f195a04e4193
LambdaSix <alexander.somerville.cox@gmail.com>
2014-07-26 22:38:35 +0100

    Move GameMap and Tile into the core assembly.
    Also rename the files in DataStructures to match their interface name.

---
+++ OctoGhast.DataStructures/Map/ITile.cs
@@ -0,0 +1,10 @@
+﻿namespace OctoGhast.DataStructures.Map
+{
+    public interface ITile
+    {
+        bool IsExplored { get; set; }
+        bool IsWalkable { get; set; }
+        int Glyph { get; set; }
+        bool IsTransparent { get; set; }
+    }
+}
