cc6b1e416e1a2d15a1149e1fe486050c30064193
LambdaSix <LambdaSix@users.noreply.github.com>
2014-07-26 22:38:35 +0100

    Move GameMap and Tile into the core assembly.
    Also rename the files in DataStructures to match their interface name.

--- OctoGhast/Map/GameMap.cs
+++ OctoGhast/Map/GameMap.cs
@@ -1,48 +1,12 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 using InfiniMap;
-using libtcod;
 using OctoGhast.DataStructures.Lighting;
-using OctoGhast.DataStructures.Renderer;
+using OctoGhast.DataStructures.Map;
 using OctoGhast.Spatial;
 
-namespace OctoGhast.DataStructures.Map
+namespace OctoGhast.Map
 {
-    public interface IGameMap
-    {
-        /// <summary>
-        /// Determine if the player has previously seen this tile.
-        /// </summary>
-        /// <param name="position">World position</param>
-        /// <returns>True if the tile has previously been seen.</returns>
-        bool IsExplored(Vec position);
-
-        /// <summary>
-        /// Determine if the tile at <paramref name="position"/> can be moved over
-        /// </summary>
-        /// <param name="position">World position</param>
-        /// <returns>True if the tile can be moved over</returns>
-        bool IsWalkable(Vec position);
-
-        /// <summary>
-        /// Determine if the tile at <paramref name="position"/> cannot be seen through
-        /// </summary>
-        /// <param name="position">World position</param>
-        /// <returns>True if the tile blocks sight</returns>
-        bool IsOpaque(Vec position);
-
-        LightMap<TileLightInfo> CalculateFov(Vec viewCenter, int lightRadius, Func<int, int, Vec> translateFunc);
-
-        Tile this[Vec pos] { get; set; }
-        Tile this[int x, int y] { get; set; }
-    }
-
     public class GameMap : IGameMap
     {
         private Map2D<Tile> _map { get; set; }
