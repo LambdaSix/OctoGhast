aa47ac922bfd06314068f3f33e3cc88936343048
LambdaSix <alexander.somerville.cox@gmail.com>
2014-08-14 22:50:51 +0100

    Modify Window to pull the screen to render from a stack.
    Each frame the main window pops the top of the Screens stack and sets that as the render target.
    Or continues rendering the old target if there isn't a new screen to render.
    
    Automatically removes controls and the old screen manager.

--- OctoGhast.UserInterface/Core/Window.cs
+++ OctoGhast.UserInterface/Core/Window.cs
@@ -77,6 +77,9 @@ namespace OctoGhast.UserInterface.Core
         internal Tooltip CurrentTooltip { get; set; }
         internal ControlBase CurrentDragging { get; set; }
 
+        protected Stack<ScreenBase> Screens { get; set; }
+        protected ScreenBase CurrentScreen { get; set; }
+
         // TODO: Replace with the application framework reference
         private Size WindowSize { get; set; }
 
@@ -267,6 +270,10 @@ namespace OctoGhast.UserInterface.Core
         public override void OnTick() {
             base.OnTick();
 
+            // Update the current screen, which may involve adding/removing controls & managers.
+            UpdateScreenState();
+            
+            // Now update managers/controls to match the current screen.
             UpdateManagers();
             UpdateControls();
 
@@ -444,6 +451,29 @@ namespace OctoGhast.UserInterface.Core
             ManagersRemoving.Clear();
         }
 
+        private void UpdateScreenState() {
+            if (Screens.Any() && Screens.Peek() != CurrentScreen) {
+                var screen = Screens.Pop();
+
+                // Old screen is about to be removed.
+                // Let them unload stuff
+                if (CurrentScreen != null) {
+                    CurrentScreen.OnTearingDown();
+                    RemoveManager(CurrentScreen);
+                    // Move all the current controls into the removal list.
+                    foreach (var control in Controls) {
+                        RemoveControl(control);
+                    }
+                }
+
+                // Add the new screen as a manager
+                // Let them setup and then register.
+                AddManager(screen);
+                screen.OnSettingUp();
+                CurrentScreen = screen;
+            }
+        }
+
         private void UpdateControls() {
             foreach (var control in ControlsPending) {
                 Controls.Add(control);
@@ -482,6 +512,10 @@ namespace OctoGhast.UserInterface.Core
                 if (CurrentTooltip != null)
                     CurrentTooltip.Dispose();
             }
+        }
+
+        public void EnqueueScreen(ScreenBase screen) {
+            Screens.Push(screen);
         }
     }
 }
