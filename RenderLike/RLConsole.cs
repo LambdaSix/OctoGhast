c91f103b1138f765d62f1c49f12fce834a25cce6
LambdaSix <LambdaSix@users.noreply.github.com>
2014-08-31 23:17:11 +0100

    Finish implementing RLConsole and Surface

--- RenderLike/RLConsole.cs
+++ RenderLike/RLConsole.cs
@@ -88,7 +88,7 @@ namespace RenderLike
                             RootSurface.Cells[x + y*RootSurface.Width].Back);
 
                         // Foreground
-                        Font.SetFontSourceRect(ref src, RootSurface.Cells[x + y*RootSurface.Width].c);
+                        Font.SetFontSourceRect(ref src, RootSurface.Cells[x + y*RootSurface.Width].Char);
                         SpriteBatch.Draw(Font.Texture,
                             new Vector2(x*Font.CharacterWidth, y*Font.CharacterHeight),
                             src,
@@ -108,6 +108,110 @@ namespace RenderLike
                 throw new ArgumentOutOfRangeException("width");
             if (height <= 0)
                 throw new ArgumentOutOfRangeException("height");
+
+            return new Surface(width, height, Font, this);
+        }
+
+        public void Blit(Surface src, Surface dst, Rectangle srcRect, int destX, int destY) {
+            if (src == null)
+                throw new ArgumentNullException("src");
+            if (dst == null)
+                throw new ArgumentNullException("dst");
+
+            var blitRect = new Rectangle(destX, destY, srcRect.Width, srcRect.Height);
+            int deltaX = srcRect.Left - blitRect.Left;
+            int deltaY = srcRect.Top - blitRect.Top;
+
+            bool dstIsRoot = dst is RootSurface;
+            var dstAsRoot = dst as RootSurface;
+
+            blitRect = Rectangle.Intersect(blitRect, new Rectangle(0, 0, dst.Width, dst.Height));
+
+            for (int y = blitRect.Top; y < blitRect.Bottom; y++) {
+                for (int x = blitRect.Left; x < blitRect.Right; x++) {
+                    int sx = deltaX + x;
+                    int sy = deltaY + y;
+
+                    dst.Cells[x + y*dst.Width].Back = src.Cells[sx + sy*src.Width].Back;
+                    dst.Cells[x + y*dst.Width].Fore = src.Cells[sx + sy*src.Width].Fore;
+                    dst.Cells[x + y*dst.Width].Char = src.Cells[sx + sy*src.Width].Char;
+
+                    if (dstIsRoot) {
+                        dstAsRoot.DirtyCells[x + y*dst.Width] = true;
+                    }
+                }
+            }
+        }
+
+        public void BlitAlpha(Surface src, Surface dst, Rectangle srcRect, int destX, int destY, float alpha) {
+            if (src == null)
+                throw new ArgumentNullException("src");
+            if (dst == null)
+                throw new ArgumentNullException("dst");
+
+            alpha = MathHelper.Clamp(alpha, 0f, 1f);
+
+            var blitRect = new Rectangle(destX, destY, srcRect.Width, srcRect.Height);
+            int deltaX = srcRect.Left - blitRect.Left;
+            int deltaY = srcRect.Top - blitRect.Top;
+
+            bool dstIsRoot = dst is RootSurface;
+            var dstAsRoot = dst as RootSurface;
+
+            blitRect = Rectangle.Intersect(blitRect, new Rectangle(0, 0, dst.Width, dst.Height));
+            Color backCol, foreCol;
+            char ch;
+
+            for (int y = blitRect.Top; y < blitRect.Bottom; y++) {
+                for (int x = blitRect.Left; x < blitRect.Right; x++) {
+                    int sx = deltaX + x;
+                    int sy = deltaY + y;
+
+                    backCol = Color.Lerp(dst.Cells[x + y*dst.Width].Back, src.Cells[sx + sy*src.Width].Back, alpha);
+
+                    if (src.Cells[sx + sy*src.Width].Char == ' ') {
+                        foreCol = Color.Lerp(dst.Cells[x + y*dst.Width].Fore, src.Cells[sx + sy*src.Width].Back, alpha);
+                        ch = dst.Cells[x + y*dst.Width].Char;
+                    }
+                    else {
+                        foreCol = src.Cells[sx + sy*src.Width].Fore;
+                        ch = src.Cells[sx + sy*src.Width].Char;
+                    }
+
+                    dst.Cells[x + y*dst.Width].Back = backCol;
+                    dst.Cells[x + y*dst.Width].Fore = foreCol;
+                    dst.Cells[x + y*dst.Width].Char = ch;
+
+                    if (dstIsRoot)
+                        dstAsRoot.DirtyCells[x + y*dst.Width] = true;
+                }
+            }
+        }
+
+        public void Blit(Surface src, Surface dst, int destX, int destY) {
+            if (src == null)
+                throw new ArgumentNullException("src");
+            if (dst == null)
+                throw new ArgumentNullException("dst");
+
+            Blit(src, dst, new Rectangle(0, 0, src.Width, src.Height), destX, destY);
+        }
+
+        public void Blit(Texture2D src, Surface dst, Rectangle srcRect, int destX, int destY) {
+            var data = new Color[srcRect.Width*srcRect.Height];
+            src.GetData(0, srcRect, data, 0, data.Length);
+
+            for (int i = 0; i < data.Length; i++) {
+                int y = i/srcRect.Width;
+                int x = i%srcRect.Width;
+                dst.PrintChar(destX + x, destY + y, (char)Font.SolidChar, data[i]);
+            }
+        }
+
+        internal void RootClear() {
+            Graphics.SetRenderTarget(RenderTarget);
+            Graphics.Clear(RootSurface.DefaultBackground);
+            Graphics.SetRenderTarget(null);
         }
     }
 }
