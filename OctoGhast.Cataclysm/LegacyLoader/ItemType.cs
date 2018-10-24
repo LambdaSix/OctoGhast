0664f633acbf11b174417ed127c47c484fcc8ea5
Nicola <LambdaSix@users.noreply.github.com>
2018-10-24 15:34:47 +0100

    Implement skeleton for Item Loading
    - Decorate all of ItemType with loader info
    - Stub out the TypeLoaders

--- OctoGhast.Cataclysm/LegacyLoader/ItemType.cs
+++ OctoGhast.Cataclysm/LegacyLoader/ItemType.cs
@@ -1,92 +1,175 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using InfiniMap;
+using Newtonsoft.Json.Linq;
 using OctoGhast.Entity;
+using OctoGhast.Framework;
 using OctoGhast.Units;
 using static OctoGhast.Translation.Translation;
 
 namespace OctoGhast.Cataclysm.LegacyLoader {
     public class ItemType {
+        [LoaderInfo("container_data", TypeLoader = typeof(ContainerTypeLoader))]
         public SlotContainer Container { get; set; }
+        
+        [LoaderInfo("tool_data", TypeLoader = typeof(ToolTypeLoader))]
         public SlotTool Tool { get; set; }
+
+        [LoaderInfo("comestible_data", TypeLoader = typeof(ComestibleTypeLoader))]
         public SlotComestible Comestible { get; set; }
+
+        [LoaderInfo("brewable", TypeLoader = typeof(BrewableTypeLoader))]
         public SlotBrewable Brewable { get; set; }
+
+        [LoaderInfo("armor_data", TypeLoader = typeof(ArmorTypeLoader))]
         public SlotArmor Armor { get; set; }
+
+        [LoaderInfo("book_data", TypeLoader = typeof(BookTypeLoader))]
         public SlotBook Book { get; set; }
+
+        [LoaderInfo("mod_data", TypeLoader = typeof(ModTypeLoader))]
         public SlotMod Mod { get; set; }
+
+        [LoaderInfo("engine_data", TypeLoader = typeof(EngineTypeLoader))]
         public SlotEngine Engine { get; set; }
+
+        [LoaderInfo("wheel_data", TypeLoader = typeof(WheelTypeLoader))]
         public SlotWheel Wheel { get; set; }
+
+        [LoaderInfo("fuel_data", TypeLoader = typeof(FuelTypeLoader))]
         public SlotFuel Fuel { get; set; }
+
+        [LoaderInfo("gun_data", TypeLoader = typeof(GunTypeLoader))]
         public SlotGun Gun { get; set; }
+
+        [LoaderInfo("gunmod_data", TypeLoader = typeof(GunModTypeLoader))]
         public SlotGunMod GunMod { get; set; }
+
+        [LoaderInfo("magazine_data", TypeLoader = typeof(MagazineTypeLoader))]
         public SlotMagazine Magazine { get; set; }
+
+        [LoaderInfo("bionic_data", TypeLoader = typeof(BionicTypeLoader))]
         public SlotBionic Bionic { get; set; }
+
+        [LoaderInfo("ammo_data", TypeLoader=typeof(AmmoTypeLoader))]
         public SlotAmmo Ammo { get; set; }
+
+        [LoaderInfo("seed_data")]
         public SlotSeed Seed { get; set; }
         // TODO: Artifacts
 
         /// <summary>
         /// String identifier for this type
         /// </summary>
+        [LoaderInfo("id", true, "null")]
         private string ID = "null";
 
+        [LoaderInfo("name", true, "none")]
         public string Name = "none";
+        [LoaderInfo("name_plural", false, "none")]
         public string PluralName = "none";
 
-        public StringID<ItemType> LooksLike { get; set; } = StringID<ItemType>.NullId;
+        [LoaderInfo("looks_like")]
+        public StringID<ItemType> LooksLike { get; set; }
+
+        [LoaderInfo("snippet_category")]
         public string SnippetCategory { get; set; }
+
+        [LoaderInfo("description")]
         public string Description { get; set; }
+
+        [LoaderInfo("container")]
         public StringID<ItemType> DefaultContainer { get; set; }
+
+        [LoaderInfo("qualities")]
         public Dictionary<string,int> Qualities { get; set; }
+
+        [LoaderInfo("properties")]
         public Dictionary<string,string> Properties { get; set; }
 
+        [LoaderInfo("materials")]
         public IEnumerable<StringID<Material>> Materials { get; set; }
 
+        [LoaderInfo("use_methods")]
         public Dictionary<string,Action> UseMethods { get; set; }
 
-        public int CountdownInterval { get; set; }= 0;
+        [LoaderInfo("countdown_interval", false, 0)]
+        public int CountdownInterval { get; set; }
 
+        [LoaderInfo("countdown_action")]
         // TODO: Implement UseMethods/UseFunction (func{t,t,t} ?)
-        public Action CountdownAction { get; set; }
+        public string CountdownAction { get; set; }
 
-        public bool CountDownDestroy { get; set; } = false;
+        [LoaderInfo("countdown_destroy", false, false)]
+        public bool CountDownDestroy { get; set; }
 
-        public Action DropAction { get; set; }
+        [LoaderInfo("drop_action")]
+        public string DropAction { get; set; }
 
+        [LoaderInfo("emits")]
         public IEnumerable<StringID<Emit>> Emits { get; set; }
 
+        [LoaderInfo("item_tags")]
         public IEnumerable<string> ItemTags { get; set; }
+
+        [LoaderInfo("techniques")]
         public IEnumerable<StringID<MaterialArtsTechnique>> Techniques { get; set; }
 
-        public int MinimumStrength { get; set; } = 0;
-        public int MinimumDexterity { get; set; } = 0;
-        public int MinimumIntelligence { get; set; } = 0;
-        public int MinimumPerception { get; set; } = 0;
+        [LoaderInfo("min_str", false, 0)]
+        public int MinimumStrength { get; set; }
+
+        [LoaderInfo("min_dex", false, 0)]
+        public int MinimumDexterity { get; set; }
+        [LoaderInfo("min_int", false, 0)]
+        public int MinimumIntelligence { get; set; }
+
+        [LoaderInfo("min_per", false, 0)]
+        public int MinimumPerception { get; set; }
+
+        [LoaderInfo("min_skills")]
         public Dictionary<StringID<Skill>, int> MinimumSkills { get; set; }
 
-        public bool ExplodesInFire { get; set; } = false;
+        [LoaderInfo("explode_in_fire", false, false)]
+        public bool ExplodesInFire { get; set; }
+
         public ExplosionData Explosion { get; set; }
 
+        [LoaderInfo("phase", false, MaterialPhase.Solid)]
         public MaterialPhase Phase { get; set; } = MaterialPhase.Solid;
 
+        [LoaderInfo("stackable", false, false)]
         public bool Stackable { get; set; } = false;
 
-        public Mass Weight { get; set; } = "0.0KG";
-        public Volume Volume { get; set; } = "0.0L";
+        [LoaderInfo("weight", true, "0.0KG")]
+        public Mass Weight { get; set; }
 
-        public int Price { get; set; } = 0;
-        public int PriceAfterEpoch { get; set; } = -1;
-        public int StackSize { get; set; } = 0;
-        public Volume IntegralVolume { get; set; } = null;
+        [LoaderInfo("volume", true, "0.0L")]
+        public Volume Volume { get; set; }
 
-        public bool Rigid { get; set; } = true;
-        public IEnumerable<int> MeleeDamageTypes { get; set; }
+        [LoaderInfo("price", false, 0)]
+        public int Price { get; set; }
+        [LoaderInfo("price_postapoc", false, 0)]
+        public int PriceAfterEpoch { get; set; }
+
+        [LoaderInfo("stack_size", false, 0)]
+        public int StackSize { get; set; }
+
+        [LoaderInfo("integral_volume", false, "-1ML")]
+        public Volume IntegralVolume { get; set; }
+
+        [LoaderInfo("rigid", false, true)]
+        public bool Rigid { get; set; }
+
+        [LoaderInfo("melee_data")]
+        public Dictionary<string,int> MeleeDamageTypes { get; set; }
+
+        [LoaderInfo("thrown_damage", TypeLoader = typeof(DamageInfoTypeLoader))]
         public DamageInfo ThrownDamage { get; set; }
 
         /// <summary>
         /// To hit bonus for melee combat, -5 to +5 is reasonable
         /// </summary>
+        [LoaderInfo("to_hit")]
         public int ToHit { get; set; } = 0;
 
         /// <summary>
@@ -94,13 +177,16 @@ namespace OctoGhast.Cataclysm.LegacyLoader {
         /// </summary>
         public int LightEmission { get; set; } = 0;
 
-        public ItemCategory Category { get; set; } = null;
+        [LoaderInfo("category")]
+        public StringID<ItemCategory> Category { get; set; } = null;
 
         /// <summary>
         /// Color on the map
         /// </summary>
+        [LoaderInfo("color")]
         public string Color { get; set; } = "white"; // TODO: Enum for default colours?
 
+        [LoaderInfo("symbol")]
         public string Symbol { get; set; } = "#";
 
         public int DamageMin { get; set; } = -1;
@@ -114,13 +200,18 @@ namespace OctoGhast.Cataclysm.LegacyLoader {
         /// <summary>
         /// Magazine types, if any, for each ammo type that can be used to reload this item.
         /// </summary>
+        [LoaderInfo("magazines")]
         public Dictionary<StringID<AmmoType>, IEnumerable<StringID<ItemType>>> Magazines { get; set; }
 
         /// <summary>
         /// Default magazine for each ammotype that can be used to reload this item.
         /// </summary>
+        /// <remarks>
+        /// This is generally the first magazine in Magazines for the given ammo-type
+        /// </remarks>
         public Dictionary<StringID<AmmoType>, StringID<ItemType>> MagazineDefault { get; set; }
 
+        [LoaderInfo("magazine_well")]
         public Volume MagazineWell { get; set; } = "0.0L";
 
         public string GetItemType() {
@@ -146,8 +237,16 @@ namespace OctoGhast.Cataclysm.LegacyLoader {
             return "MISC";
         }
 
+        public StringID<ItemType> GetDefaultMagazine(StringID<AmmoType> caliber) {
+            if (Magazines.TryGetValue(caliber, out var defaultMagazine)) {
+                return defaultMagazine.First();
+            }
+
+            return StringID<ItemType>.NullId;
+        }
+
         public string GetName(int quantity = 1) {
-            return Translation.Translation._($"{Name}", $"{PluralName}", quantity);
+            return _($"{Name}", $"{PluralName}", quantity);
         }
 
         public string GetId() => ID;
--- OctoGhast.Cataclysm/LegacyLoader/TypeLoaders.cs
+++ OctoGhast.Cataclysm/LegacyLoader/TypeLoaders.cs
@@ -45,4 +45,184 @@ namespace OctoGhast.Cataclysm.LegacyLoader {
             return Load(data, existing as SlotTool);
         }
     }
+
+    public class DamageInfoTypeLoader : ITypeLoader<DamageInfo> {
+        /// <inheritdoc />
+        public DamageInfo Load(JObject jObj, DamageInfo existing = default(DamageInfo)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ComestibleTypeLoader : ITypeLoader<SlotComestible> {
+        /// <inheritdoc />
+        public SlotComestible Load(JObject jObj, SlotComestible existing = default(SlotComestible)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BrewableTypeLoader : ITypeLoader<SlotBrewable> {
+        /// <inheritdoc />
+        public SlotBrewable Load(JObject jObj, SlotBrewable existing = default(SlotBrewable)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ArmorTypeLoader : ITypeLoader<SlotArmor> {
+        /// <inheritdoc />
+        public SlotArmor Load(JObject jObj, SlotArmor existing = default(SlotArmor)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BookTypeLoader : ITypeLoader<SlotBook> {
+        /// <inheritdoc />
+        public SlotBook Load(JObject jObj, SlotBook existing = default(SlotBook)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ModTypeLoader : ITypeLoader<SlotMod> {
+        /// <inheritdoc />
+        public SlotMod Load(JObject jObj, SlotMod existing = default(SlotMod)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EngineTypeLoader : ITypeLoader<SlotEngine> {
+        /// <inheritdoc />
+        public SlotEngine Load(JObject jObj, SlotEngine existing = default(SlotEngine)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WheelTypeLoader : ITypeLoader<SlotWheel> {
+        /// <inheritdoc />
+        public SlotWheel Load(JObject jObj, SlotWheel existing = default(SlotWheel)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FuelTypeLoader : ITypeLoader<SlotFuel> {
+        /// <inheritdoc />
+        public SlotFuel Load(JObject jObj, SlotFuel existing = default(SlotFuel)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GunTypeLoader : ITypeLoader<SlotGun> {
+        /// <inheritdoc />
+        public SlotGun Load(JObject jObj, SlotGun existing = default(SlotGun)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GunModTypeLoader : ITypeLoader<SlotGunMod> {
+        /// <inheritdoc />
+        public SlotGunMod Load(JObject jObj, SlotGunMod existing = default(SlotGunMod)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MagazineTypeLoader : ITypeLoader<SlotMagazine> {
+        /// <inheritdoc />
+        public SlotMagazine Load(JObject jObj, SlotMagazine existing = default(SlotMagazine)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+             throw new NotImplementedException();
+        }
+    }
+
+    public class BionicTypeLoader : ITypeLoader<SlotBionic> {
+        /// <inheritdoc />
+        public SlotBionic Load(JObject jObj, SlotBionic existing = default(SlotBionic)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AmmoTypeLoader : ITypeLoader<SlotAmmo> {
+        /// <inheritdoc />
+        public SlotAmmo Load(JObject jObj, SlotAmmo existing = default(SlotAmmo)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SeedTypeLoader : ITypeLoader<SlotSeed> {
+        /// <inheritdoc />
+        public SlotSeed Load(JObject jObj, SlotSeed existing = default(SlotSeed)) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object Load(JObject data, object existing) {
+            throw new NotImplementedException();
+        }
+    }
 }
