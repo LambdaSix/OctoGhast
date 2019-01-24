6cb8aa4433618e6dd95e7f9960883ba4ed440f12
Nicola <LambdaSix@users.noreply.github.com>
2019-01-24 17:41:06 +0000

    Server skeleton project

---
+++ OctoGhast.Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace OctoGhast.Server
+{
+    public class ServerConstants
+    {
+        public static double TickRate = TimeSpan.FromMilliseconds(20).TotalMilliseconds;
+        public static double TickTime = 1000 / TickRate;
+        public static double CombatTickRate = TimeSpan.FromSeconds(1).TotalMilliseconds;
+        public static int TicksPerSecond = 20;
+    }
+
+    class Program
+    {
+        public static DateTime CurrentTime { get; set; }
+        public static bool _closing;
+
+
+        static void Main(string[] args) {
+            var worldPath = "./DefaultWorld/";
+
+            var World = new World(worldPath);
+
+            var worldTick = 0UL;
+            var now = DateTime.UtcNow;
+
+            while (!_closing)
+            {
+                // Process game loop
+                World.Tick(worldTick);
+
+                // Process background services and such
+
+                var nextTickDue = false;
+
+                while (!nextTickDue)
+                {
+                    var elapsedTime = DateTime.UtcNow;
+                    var span = elapsedTime - CurrentTime;
+
+                    if (span.TotalMilliseconds > 1000 / ServerConstants.TicksPerSecond)
+                        nextTickDue = false;
+                    else
+                    {
+                        int sleepTime = (int)((1000 / ServerConstants.TicksPerSecond) - span.TotalMilliseconds);
+                        Thread.Sleep(sleepTime);
+                        nextTickDue = true;
+                    }
+                }
+
+                CurrentTime = DateTime.UtcNow;
+            }
+
+        }
+    }
+}
---
+++ OctoGhast.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OctoGhast.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OctoGhast.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("252ee9dd-0dec-4be0-a159-f26cd198531d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
