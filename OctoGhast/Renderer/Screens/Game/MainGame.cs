908b98e0b87b48f41ff33a500669f8c69d50e747
LambdaSix <LambdaSix@users.noreply.github.com>
2026-09-26 16:29:01 +0100

    Issue #53: remove legacy OO runtime dependency

--- OctoGhast/Renderer/Screens/Game/MainGame.cs
+++ OctoGhast/Renderer/Screens/Game/MainGame.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Remoting.Messaging;
-using OctoGhast.Entity;
 using OctoGhast.Map;
 using OctoGhast.MapGeneration.Dungeons;
 using OctoGhast.Renderer.Screens.Game.Controls;
@@ -9,8 +6,6 @@ using OctoGhast.Renderer.View;
 using OctoGhast.Spatial;
 using OctoGhast.UserInterface.Controls;
 using OctoGhast.UserInterface.Core;
-using OctoGhast.UserInterface.Core.Messages;
-using XColor = Microsoft.Xna.Framework.Color;
 
 namespace OctoGhast.Renderer.Screens
 {
@@ -18,60 +13,55 @@ namespace OctoGhast.Renderer.Screens
     {
         private IMapViewModel MapModel { get; set; }
 
-        public MainGame() {
-            RegisterAction((int)GameActions.GameMap_MoveNorth, () => MapModel.Player.MoveTo(MapModel.Player.Position.Offset(0, -1)));
-            RegisterAction((int)GameActions.GameMap_MoveSouth, () => MapModel.Player.MoveTo(MapModel.Player.Position.Offset(0, +1)));
-            RegisterAction((int)GameActions.GameMap_MoveLeft, () => MapModel.Player.MoveTo(MapModel.Player.Position.Offset(-1, 0)));
-            RegisterAction((int)GameActions.GameMap_MoveRight, () => MapModel.Player.MoveTo(MapModel.Player.Position.Offset(+1, 0)));
-            RegisterAction((int) GameActions.GameMap_ShowLighting, () => MapModel.DrawLighting = !MapModel.DrawLighting);
+        public MainGame()
+        {
+            RegisterAction((int)GameActions.GameMap_ShowLighting,
+                () => MapModel.DrawLighting = !MapModel.DrawLighting);
         }
 
-        public override void OnSettingUp() {
+        public override void OnSettingUp()
+        {
             base.OnSettingUp();
 
             var windowSize = ParentWindow.ParentApplication.CurrentWindow.Size;
 
-            MapModel = new MapViewModel()
+            MapModel = new MapViewModel
             {
                 Camera = new Camera(Vec.Zero, windowSize),
                 Map = new GameMap(windowSize.Height, windowSize.Width),
-                Player = new Player(Vec.Zero, '@', new Color(XColor.Orange)),
+                PlayerPosition = Vec.Zero
             };
-
-            MapModel.Camera.BindTo(MapModel.Player);
 
             var mapGenerator = new BSPDungeonGenerator();
-            mapGenerator.PlayerPlacementFunc = rect => {
+            mapGenerator.PlayerPlacementFunc = rect =>
+            {
                 Console.WriteLine("Putting player at {0}", rect.Center);
-                MapModel.Player.MoveTo(rect.Center);
+                MapModel.PlayerPosition = rect.Center;
+                MapModel.Camera.MoveTo(rect.Center);
             };
             mapGenerator.MobilePlacementFunc =
-                rect => Console.WriteLine("Wanted to place a Mobile at ({0})", rect.TopLeft.ToString());
-            mapGenerator.TileFactory = () => new Tile {Glyph = '#'};
+                rect => Console.WriteLine("Wanted to place a Mobile at ({0})", rect.TopLeft);
+            mapGenerator.TileFactory = () => new Tile { Glyph = '#' };
 
             mapGenerator.GenerateMap(new Size(70, 70));
 
-            for (int x = 0; x < mapGenerator.Dimensions.Bounds.TopRight.X; x++) {
-                for (int y = 0; y < mapGenerator.Dimensions.Bounds.BottomLeft.Y; y++) {
+            for (int x = 0; x < mapGenerator.Dimensions.Bounds.TopRight.X; x++)
+            {
+                for (int y = 0; y < mapGenerator.Dimensions.Bounds.BottomLeft.Y; y++)
+                {
                     MapModel.Map[x, y] = mapGenerator.Map[x, y];
                 }
             }
 
-            /* Control Templates */
-            var mapTemplate = new GameMapControlTemplate()
+            var mapTemplate = new GameMapControlTemplate
             {
                 Model = MapModel,
                 Size = windowSize
             };
 
-            /* Control widgets */
-
             var mapControl = new GameMapControl(mapTemplate);
 
-            ParentWindow.AddControls(new ControlBase[]
-            {
-                mapControl
-            });
+            ParentWindow.AddControls(new ControlBase[] { mapControl });
         }
     }
 }
