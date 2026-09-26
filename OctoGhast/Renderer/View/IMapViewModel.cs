3fd9ddf492c209e50a608302543f1aad2acb0b81
LambdaSix <LambdaSix@users.noreply.github.com>
2026-09-26 16:28:58 +0100

    Issue #53: remove legacy OO runtime dependency

--- OctoGhast/Renderer/View/IMapViewModel.cs
+++ OctoGhast/Renderer/View/IMapViewModel.cs
@@ -1,14 +1,13 @@
 ﻿using OctoGhast.DataStructures.Lighting;
 using OctoGhast.DataStructures.Map;
 using OctoGhast.DataStructures.Renderer;
-using OctoGhast.Entity;
 using OctoGhast.Spatial;
 
 namespace OctoGhast.Renderer.View
 {
     public interface IMapViewModel
     {
-        IPlayer Player { get; set; }
+        Vec PlayerPosition { get; set; }
         ICamera Camera { get; set; }
         IGameMap Map { get; set; }
         bool DrawLighting { get; set; }
